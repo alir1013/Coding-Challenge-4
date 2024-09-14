71d2f2b33b88d2d2d650d8b668e3d4c28c06cc5c
Roan Ali <alir13@usf.edu>
2024-09-14 12:18:27 -0400

    Initialize inventory with product objects

---
+++ inventory_management.js
@@ -0,0 +1,13 @@
+//Task 1:Creating an Inventory Array of Product Objects 
+//Creating an array of 5 Products 
+const inventory= [
+    {name: "Cotton Candy",price:2.75,quantity:27,lowStockLevel:6}, //Properties of Cotton Candy
+    {name: "Hershey Chocolate",price:4.23,quantity:40,lowStockLevel:20}, //Properties of Hersheys Chocolate 
+    {name:"Gum",price:1.50,quantity:56,lowStockLevel:25},//properties of Gum
+    {name:"Fruit Leathers",price:6.15,quantity:36,lowStockLevel:16},//Properties of Fruit Leather
+    {name:"Gummy Bears",price:3.50,quantity:32,lowStockLevel:15}//Properties of Gummy Bears
+ 
+ ];
+ 
+ console.log("Inventory:",(inventory)); //Logging Array of Inventory details 
+ 
